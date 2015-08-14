f51d6ba73872b6226fb825d52857648125c1147c
Georg Steinmetz <georg.steinmetz@1und1.de>
2015-08-14 10:07:49 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Upload to Hastebin
+
+This is a plugin for Sublime Text 2&3 that will upload your document or selection to a hastebin server.
+
+## Usage
+
+You can either use the combination `ctrl+alt+h`, make a right click or use the command palette and search for `Upload to Hastebin`. If you made any selection only that part of the document will be uploaded.
---
+++ sublime-hastebin.py
@@ -0,0 +1,37 @@
+import sublime, sublime_plugin
+import json
+import webbrowser
+# import urllib
+try:
+  import urllib2 as urllib
+except ImportError:
+  import urllib.request as urllib
+# import urljoin
+try:
+  from urlparse import urljoin
+except (ImportError) as e:
+  from urllib.parse import urljoin
+
+class UploadToHastebin( sublime_plugin.TextCommand ):
+  def run(self, view):
+    settings = sublime.load_settings("sublime-hastebin.sublime-settings")
+    hastebinUrl = settings.get("hastebin-url")
+    if not "document" in hastebinUrl:
+      hastebinUrl = urljoin(hastebinUrl, "documents")
+
+    for region in self.view.sel():
+      if not region.empty():
+        document = self.view.substr(region).encode("utf8")
+      else:
+        document = self.view.substr(sublime.Region(0, self.view.size())).encode("utf8")
+
+      request = urllib.Request(hastebinUrl, document)
+      response = urllib.urlopen(request)
+      body = response.read()
+      documentKey = json.loads(body.decode("utf8"))["key"]
+      url = urljoin(hastebinUrl, documentKey)
+
+      if settings.get("copy-to-clipboard"):
+        sublime.set_clipboard(url)
+      if settings.get("open-in-browser"):
+        webbrowser.open_new_tab(url)
