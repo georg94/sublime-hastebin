11ecbd2258f5167376fd47be1e43717301a655c0
Georg Steinmetz <georg.steinmetz@1und1.de>
2015-08-14 11:26:55 +0200

    added comments and fixed problem that multiselections did not get uploaded accurate

--- sublime-hastebin.py
+++ sublime-hastebin.py
@@ -14,31 +14,52 @@ except (ImportError) as e:
 
 class UploadToHastebin( sublime_plugin.TextCommand ):
   def run(self, view):
+    # load settings
     settings = sublime.load_settings("sublime-hastebin.sublime-settings")
+
+    # and get hastebin URL
     hastebinUrl = settings.get("hastebin-url")
+
+    # if the user only enters a hostname without "http[s]://" I'll add "http://" for him
     if not (hastebinUrl.startswith("http://") or hastebinUrl.startswith("https://")):
       hastebinUrl = "http://" + hastebinUrl
+
+    # if the user only entered the url to hastebin without "documents" that will be fixed
+    # tbh I also leave "documets" away in the config
     if not "document" in hastebinUrl:
       hastebinUrl = urljoin(hastebinUrl, "documents")
 
+    # check if something is selected
+
+    document = ""
     for region in self.view.sel():
       if not region.empty():
-        document = self.view.substr(region).encode("utf8")
+        # there is a selection, only upload that
+        document += "\n" + str(self.view.substr(region))
       else:
-        document = self.view.substr(sublime.Region(0, self.view.size())).encode("utf8")
+        # no selection -> uplaod the whole document
+        document = self.view.substr(sublime.Region(0, self.view.size()))
 
-      request = urllib.Request(hastebinUrl, document)
-      response = urllib.urlopen(request)
-      body = response.read()
+    document = document.encode("utf8")
 
-      documentKey = json.loads(body.decode("utf8"))["key"]
+    # now do the upload
+    request = urllib.Request(hastebinUrl, document)
+    response = urllib.urlopen(request)
+    body = response.read()
 
-      window = self.view.window()
-      if "file_extension" in window.extract_variables():
-        documentKey += "."+window.extract_variables()["file_extension"]
-      url = urljoin(hastebinUrl, documentKey)
+    # get key from answer body
+    documentKey = json.loads(body.decode("utf8"))["key"]
 
-      if settings.get("copy-to-clipboard"):
-        sublime.set_clipboard(url)
-      if settings.get("open-in-browser"):
-        webbrowser.open_new_tab(url)
+    # see if the current file has a extension, if yes append it to the key.
+    # That will make hastebin highlight it in the right syntax.
+    window = self.view.window()
+    if "file_extension" in window.extract_variables():
+      documentKey += "."+window.extract_variables()["file_extension"]
+    url = urljoin(hastebinUrl, documentKey)
+
+    # copy to clipboard if enabled
+    if settings.get("copy-to-clipboard"):
+      sublime.set_clipboard(url)
+    # open in browser if enabled
+    if settings.get("open-in-browser"):
+      webbrowser.open_new_tab(url)
